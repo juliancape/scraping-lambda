0727353399fafe12b415b25aaaeb77e617f30492
juliancape <73551229+juliancape@users.noreply.github.com>
2024-02-21 10:36:32 -0500

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -4,34 +4,27 @@ from datetime import datetime
 import urllib.request
 
 def lambda_handler(event, context):
-    current_datetime = datetime.utcnow()
+    current_datetime = datetime.now().strftime('%Y-%m-%d')
 
-    if current_datetime.hour == 20 and current_datetime.minute == 30:
-        eltiempo_url = 'https://www.eltiempo.com/'
+    eltiempo_url = 'https://www.eltiempo.com/'
+    ruq = urllib.request.Request(eltiempo_url, headers={'User-agent': 'Mozilla/5.0'})    
+    with urllib.request.urlopen(ruq) as response:
+        page_content = response.read().decode('utf-8')
 
-        with urllib.request.urlopen(eltiempo_url) as response:
-            page_content = response.read().decode('utf-8')
+    if response.code == 200:
+        filename = f'{current_datetime}.html'
 
-        if response.code == 200:
-            filename = f'{current_datetime.strftime('%Y-%m-%d')}.html'
+        s3 = boto3.client('s3', region_name='us-east-1')
 
-            s3 = boto3.client('s3', region_name='us-east-1')
-
-            s3.put_object(Body=page_content, Bucket='eltiempo-julian', Key=filename)
-            print('Ejecutado')
-            return {
-                'statusCode': 200,
-                'body': json.dumps(f'Se ha descargado y guardado la página del tiempo en {filename}')
-            }
-        else:
-            print('Error')
-            return {
-                'statusCode': response.code,
-                'body': json.dumps(f'Error al descargar la página del tiempo. Código de estado: {response.code}')
-            }
-    else:
-        print('Sin ejecutar')
+        s3.put_object(Body=page_content, Bucket='eltiempo-julian', Key=filename)
+        print('Ejecutado')
         return {
             'statusCode': 200,
-            'body': json.dumps('La función no se ejecutó en este momento.')
+            'body': json.dumps(f'Se ha descargado y guardado la página del tiempo en {filename}')
+        }
+    else:
+        print('Error')
+        return {
+            'statusCode': response.code,
+            'body': json.dumps(f'Error al descargar la página del tiempo. Código de estado: {response.code}')
         }
